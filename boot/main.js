63ad5ecd29c887f459e7ff858ff9a0c921e8c127
denis0001-dev <denis0001.dev@ya.ru>
2024-06-11 19:08:39 +0300

    Boot function

---
+++ boot/main.js
@@ -0,0 +1,11 @@
+async function boot() {
+    const loading_screen = new LoadingScreen(document.getElementById("loading_screen"));
+    loading_screen.start();
+    await delay(5000);
+
+    loading_screen.spinner.stop();
+
+    await delay(1000);
+
+    loading_screen.stop();
+}
