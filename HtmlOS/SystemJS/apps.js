81c77c3d7e7f92009a1f065c154d0c8064e06605
denis0001-dev <demorozov2048@gmail.com>
2024-07-13 14:39:16 +0300

    Исправление багов

--- HtmlOS/SystemJS/apps.js
+++ HtmlOS/SystemJS/apps.js
@@ -1,11 +1,28 @@
 class CommandLine extends DesktopWindow {
-    constructor(title, width, height) {
-        super(title, width, height, true, true, true, "command_prompt", null);
+    constructor(width, height) {
+        super(
+            "Command Prompt",
+            width,
+            height,
+            true,
+            true,
+            true,
+            "command_prompt",
+            null
+        );
 
         const content = this.content;
 
         const iframe = document.createElement("iframe");
         iframe.src = "HtmlOS/SystemJS/cmd/index.html";
+
+        iframe.sandbox.add(...[
+            "allow-scripts",
+            "allow-same-origin",
+            "allow-popups",
+            "allow-forms",
+            "allow-downloads",
+        ]);
         content.appendChild(iframe);
         this.content = content;
     }
