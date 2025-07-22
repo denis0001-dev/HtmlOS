a863d3431ef81ca681cfad0a16b0fc8de68e849b
denis0001-dev <denis0001.dev@ya.ru>
2025-07-22 12:27:21 +0300

    Optimize build
    Signed-off-by: denis0001-dev <denis0001.dev@ya.ru>

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
   root: '.',
-  base: './', // <-- This makes all asset URLs relative to index.html
-  plugins: [react()],
+  base: './',
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true, // Enable HTML minification
+    }),
+  ],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
